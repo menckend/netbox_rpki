85be3806e594590adc327c046eaef217546f8a30
Mencken Davidson <76002183+menckend@users.noreply.github.com>
2024-10-30 19:14:57 -0400

    Update urls.py

--- netbox_rpki/urls.py
+++ netbox_rpki/urls.py
@@ -8,9 +8,9 @@ app_name = 'netbox_rpki'
 urlpatterns = [
     # RpkiCertificate
     path('certificates/', views.RpkiCertificateListView.as_view(), name='rpkicertificate_list'),
-#    path('certificates/add/', views.RpkiCertificateEditView.as_view(), name='cpkicertificate_add'),
-#    path('certificates/<int:pk>/', views.RpkiCertificate.as_view(), name='RpkiCertificate'),
-#    path('certificates/<int:pk>/edit/', views.RpkiCertificateEditView.as_view(), name='RpkiCertificate_edit'),
-#    path('certificates/<int:pk>/delete/', views.RpkiCertificateDeleteView.as_view(), name='RpkiCertificate_delete'),
+    path('certificates/add/', views.RpkiCertificateEditView.as_view(), name='rpkicertificate_add'),
+#    path('certificates/<int:pk>/', views.RpkiCertificate.as_view(), name='rpkicertificate'),
+#    path('certificates/<int:pk>/edit/', views.RpkiCertificateEditView.as_view(), name='rpkicertificate_edit'),
+#    path('certificates/<int:pk>/delete/', views.RpkiCertificateDeleteView.as_view(), name='rpkicertificate_delete'),
 #    path('certificates/<int:pk>/', include(get_model_urls('netbox_rpki', 'rpkicertificate'))),
 ]
