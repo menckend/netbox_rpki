b61f75dda54d012a7b69fa15ee7ca3f096954c0e
Mencken Davidson <76002183+menckend@users.noreply.github.com>
2024-10-30 22:57:35 -0400

    Update tables.py

--- netbox_rpki/tables.py
+++ netbox_rpki/tables.py
@@ -2,39 +2,38 @@
 import django_tables2 as tables
 from netbox.tables import NetBoxTable, ChoiceFieldColumn
 import netbox_rpki
-from netbox_rpki.models import RpkiCertificate, RpkiOrganization, RpkiRoa, RpkiRoaPrefices
+from netbox_rpki.models import certificate, organization, roa, roaprefices
 
 
-class RpkiCertificateTable(NetBoxTable):
+class certificateTable(NetBoxTable):
     name = tables.Column(linkify=True)
 
     class Meta(NetBoxTable.Meta):
-        model = netbox_rpki.models.RpkiCertificate
+        model = netbox_rpki.models.certificate
         fields = ("pk", "id", "name", "issuer", "subject", "serial", "validFrom", "validTo", "publicKey", "privateKey", "publicationURL", "caRepository", "selfHosted", "rpkiOrg")
         default_columns = ("name", "issuer", "subject", "serial", "validFrom", "validTo", "publicKey", "privateKey", "publicationURL", "caRepository", "selfHosted", "rpkiOrg")
 
-class RpkiOrganizationTable(NetBoxTable):
+class organizationTable(NetBoxTable):
     name = tables.Column(linkify=True)
 
     class Meta(NetBoxTable.Meta):
-        model = netbox_rpki.models.RpkiOrganization
+        model = netbox_rpki.models.organization
         fields = ("pk", "id", "orgId", "orgName")
         default_columns = ("orgName",)
 
-class RpkiRoaTable(NetBoxTable):
+class roaTable(NetBoxTable):
     name = tables.Column(linkify=True)
 
     class Meta(NetBoxTable.Meta):
-        model = netbox_rpki.models.RpkiRoa
+        model = netbox_rpki.models.roa
         fields = ("pk", "id", 'name', "originAs", "validFrom", "validTo", "signedBy")
         default_columns = ("name", "originAs", "validFrom", "validTo", "signedBy")
 
 
-class RpkiRoaPreficesTable(NetBoxTable):
+class roapreficesTable(NetBoxTable):
     name = tables.Column(linkify=True)
 
     class Meta(NetBoxTable.Meta):
-        model = netbox_rpki.models.RpkiRoaPrefices
+        model = netbox_rpki.models.roaprefices
         fields = ("pk", "id", "prefix", "maxLength", "roaName")
         default_columns = ("prefix", "maxLength", "roaName")
-
