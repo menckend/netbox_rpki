427a92f595b1ddbf73c6b4efd25c0cc560d6f085
Mencken Davidson <76002183+menckend@users.noreply.github.com>
2024-10-31 11:16:14 -0400

    Update urls.py

--- netbox_rpki/api/urls.py
+++ netbox_rpki/api/urls.py
@@ -1,7 +1,7 @@
 """Django API url router definitions for the netbox_ptov plugin"""
 
 from netbox.api.routers import NetBoxRouter
-
+import netbox_rpki
 from netbox_rpki.api.views import (
     CertificateViewSet, OrganizationViewSet, RoaViewSet, RoaPrefixViewSet, RootView
 )
@@ -10,9 +10,9 @@ app_name = 'netbox_rpki'
 
 router = NetBoxRouter()
 router.APIRootView = RootView
-router.register('certificate', CertificateViewSet, basename='certificate')
-router.register('organization', OrganizationViewSet, basename='organization')
-router.register('roa', RoaViewSet, basename='roa')
+router.register('certificate', netbox_rpki.aapi.views.CertificateViewSet, basename='certificate')
+router.register('organization', netbox_rpki.api.views.OrganizationViewSet, basename='organization')
+router.register('roa', netbox_rpki.api.views.RoaViewSet, basename='roa')
 router.register('roaprefix', RoaprefixViewSet, basename='roaprefix')
 
 urlpatterns = router.urls
