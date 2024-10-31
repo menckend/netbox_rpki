1c721f6a97d144ccb5cdf38b9c83accacb6e317c
Mencken Davidson <76002183+menckend@users.noreply.github.com>
2024-10-30 23:00:54 -0400

    Update forms.py

--- netbox_rpki/forms.py
+++ netbox_rpki/forms.py
@@ -3,23 +3,23 @@ from ipam.models import Prefix
 from netbox.forms import NetBoxModelForm
 # from utilities.forms.fields import CommentField, DynamicModelChoiceField
 from dcim.models import devices
-from netbox_rpki.models import RpkiCertificate, RpkiOrganization, RpkiRoa, RpkiRoaPrefices
+from netbox_rpki.models import certificate, organization, roa, roaprefices
 
 
-class RpkiCertificateForm(NetBoxModelForm):
-    model = RpkiCertificate
+class certificateForm(NetBoxModelForm):
+    model = certificate
     fields = ("name", "issuer", "subject", "serial", " validFrom", "validTo", "publicKey", "privateKey", "publicationUrl", "caRepository", "OrgID", "selfHosted")
 
 
-class RpkiOrganizationForm(NetBoxModelForm):
-    model = RpkiOrganization
+class organizationForm(NetBoxModelForm):
+    model = organization
     fields = ("orgId", "orgName")
 
-class RpkiRoaForm(NetBoxModelForm):
-    model = RpkiRoa
+class roaForm(NetBoxModelForm):
+    model = roa
     fields = ("name", "originAs", "validFrom", "validTo", "signedBy")
 
 
-class RpkiRoaPreficesForm(NetBoxModelForm):
-    model = RpkiRoaPrefices
+class roapreficesForm(NetBoxModelForm):
+    model = roaprefices
     fields = ("prefix", "maxLength", "roaName")
