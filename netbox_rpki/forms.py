36cea2f88dc93ccedacd9bbb416420b6ee0f989d
Mencken Davidson <76002183+menckend@users.noreply.github.com>
2024-10-31 12:59:50 -0400

    Update forms.py

--- netbox_rpki/forms.py
+++ netbox_rpki/forms.py
@@ -9,37 +9,36 @@ from netbox_rpki.models import Certificate, Organization, Roa, RoaPrefix
 
 class CertificateForm(NetBoxModelForm):
     model = netbox_rpki.models.Certificate
-    fields = ["name", "issuer", "subject", "serial", " valid_from", "valid_to", "public_key", "private_key", "publication_url", "ca_repository", "org_id", "self_hosted"]
 
     class Meta:
         model = netbox_rpki.models.Certificate
-#        fields = ["name", "issuer", "subject", "serial", " valid_from", "valid_to", "public_key", "private_key", "publication_url", "ca_repository", "org_id", "self_hosted"]
+        fields = ["name", "issuer", "subject", "serial", " valid_from", "valid_to", "public_key", "private_key", "publication_url", "ca_repository", "org_id", "self_hosted"]
 
 
 class OrganizationForm(NetBoxModelForm):
     model = netbox_rpki.models.Organization
-    fields = ["org_id", "name"]
+#    fields = ["org_id", "name"]
 
     class Meta:
         model = netbox_rpki.models.Organization
-#        fields = ["org_id", "name"]
+        fields = ["org_id", "name"]
 
 
 class RoaForm(NetBoxModelForm):
     model = netbox_rpki.models.Roa
-    fields = ["name", "origin_as", "valid_from", "valid_to", "signed_by"]
+#    fields = ["name", "origin_as", "valid_from", "valid_to", "signed_by"]
 
     class Meta:
         model = netbox_rpki.models.Roa
-#        fields = ["name", "origin_as", "valid_from", "valid_to", "signed_by"]
+        fields = ["name", "origin_as", "valid_from", "valid_to", "signed_by"]
 
 
 
 class RoaPrefixForm(NetBoxModelForm):
     model = netbox_rpki.models.RoaPrefix
-    fields = ["prefix", "max_length", "roa_name"]
+#    fields = ["prefix", "max_length", "roa_name"]
 
     class Meta:
         model = netbox_rpki.models.RoaPrefix
-#        fields = ["prefix", "max_length", "roa_name"]
+        fields = ["prefix", "max_length", "roa_name"]
         
