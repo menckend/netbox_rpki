38fffd19a66ae004cde083f818d47c3d0cd6ca24
Mencken Davidson <76002183+menckend@users.noreply.github.com>
2024-10-30 20:50:25 -0400

    Update urls.py

--- netbox_rpki/urls.py
+++ netbox_rpki/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('certificates/<int:pk>/', include(get_model_urls('netbox_rpki', 'rpkicertificate'))),
     # RpkiOrganization
     path('orgs/', views.RpkiOrganizationListView.as_view(), name='rpkiOrganization_list'),
-#    path('orgs/add/', views.RpkiOrganizationEditView.as_view(), name='rpkiOrganization_add'),
+    path('orgs/add/', views.RpkiOrganizationEditView.as_view(), name='rpkiOrganization_add'),
 #    path('orgs/<int:pk>/', views.RpkiOrganizationView.as_view(), name='rpkiOrganization'),
 #    path('orgs/<int:pk>/edit/', views.RpkiOrganizationEditView.as_view(), name='rpkiOrganization_edit'),
 #    path('orgs/<int:pk>/delete/', views.RpkiOrganizationDeleteView.as_view(), name='rpkiOrganization_delete'),
