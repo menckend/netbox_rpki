0a431c70e14f8680c2422aaaf4800ba9d5d662f5
Mencken Davidson <76002183+menckend@users.noreply.github.com>
2024-10-31 13:03:27 -0400

    Update forms.py

--- netbox_rpki/forms.py
+++ netbox_rpki/forms.py
@@ -8,7 +8,6 @@ from netbox_rpki.models import Certificate, Organization, Roa, RoaPrefix
 
 
 class CertificateForm(NetBoxModelForm):
-    model = netbox_rpki.models.Certificate
 
     class Meta:
         model = netbox_rpki.models.Certificate
@@ -16,8 +15,6 @@ class CertificateForm(NetBoxModelForm):
 
 
 class OrganizationForm(NetBoxModelForm):
-    model = netbox_rpki.models.Organization
-#    fields = ["org_id", "name"]
 
     class Meta:
         model = netbox_rpki.models.Organization
@@ -25,8 +22,6 @@ class OrganizationForm(NetBoxModelForm):
 
 
 class RoaForm(NetBoxModelForm):
-    model = netbox_rpki.models.Roa
-#    fields = ["name", "origin_as", "valid_from", "valid_to", "signed_by"]
 
     class Meta:
         model = netbox_rpki.models.Roa
@@ -35,8 +30,6 @@ class RoaForm(NetBoxModelForm):
 
 
 class RoaPrefixForm(NetBoxModelForm):
-    model = netbox_rpki.models.RoaPrefix
-#    fields = ["prefix", "max_length", "roa_name"]
 
     class Meta:
         model = netbox_rpki.models.RoaPrefix
