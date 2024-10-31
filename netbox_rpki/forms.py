6c3f4a46ef2618cfeb971386b7248436cc70a830
Mencken Davidson <76002183+menckend@users.noreply.github.com>
2024-10-31 12:45:14 -0400

    Update forms.py

--- netbox_rpki/forms.py
+++ netbox_rpki/forms.py
@@ -11,16 +11,35 @@ class CertificateForm(NetBoxModelForm):
     model = netbox_rpki.models.Certificate
     fields = ["name", "issuer", "subject", "serial", " valid_from", "valid_to", "public_key", "private_key", "publication_url", "ca_repository", "org_id", "self_hosted"]
 
+    class Meta:
+        model = netbox_rpki.models.Certificate
+        fields = ["name", "issuer", "subject", "serial", " valid_from", "valid_to", "public_key", "private_key", "publication_url", "ca_repository", "org_id", "self_hosted"]
+
 
 class OrganizationForm(NetBoxModelForm):
     model = netbox_rpki.models.Organization
     fields = ["org_id", "name"]
 
+    class Meta:
+        model = netbox_rpki.models.Organization
+        fields = ["org_id", "name"]
+
+
 class RoaForm(NetBoxModelForm):
     model = netbox_rpki.models.Roa
     fields = ["name", "origin_as", "valid_from", "valid_to", "signed_by"]
+
+    class Meta:
+        model = netbox_rpki.models.Roa
+        fields = ["name", "origin_as", "valid_from", "valid_to", "signed_by"]
+
 
 
 class RoaPrefixForm(NetBoxModelForm):
     model = netbox_rpki.models.RoaPrefix
     fields = ["prefix", "max_length", "roa_name"]
+
+    class Meta:
+        model = netbox_rpki.models.RoaPrefix
+        fields = ["prefix", "max_length", "roa_name"]
+        
