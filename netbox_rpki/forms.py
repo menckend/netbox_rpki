3c10c7310ea916aab415f6cbc1e1f19580d9e8f6
Mencken Davidson <76002183+menckend@users.noreply.github.com>
2024-10-31 00:38:05 -0400

    Update forms.py

--- netbox_rpki/forms.py
+++ netbox_rpki/forms.py
@@ -1,25 +1,26 @@
+import netbox_rpki
 from django import forms
 from ipam.models import Prefix
 from netbox.forms import NetBoxModelForm
-# from utilities.forms.fields import CommentField, DynamicModelChoiceField
+from utilities.forms.fields import CommentField, DynamicModelChoiceField
 from dcim.models import devices
 from netbox_rpki.models import certificate, organization, roa, roaprefices
 
 
 class certificateForm(NetBoxModelForm):
     model = certificate
-    fields = ("name", "issuer", "subject", "serial", " validFrom", "validTo", "publicKey", "privateKey", "publicationUrl", "caRepository", "OrgID", "selfHosted")
+    fields = ["name", "issuer", "subject", "serial", " validFrom", "validTo", "publicKey", "privateKey", "publicationUrl", "caRepository", "OrgID", "selfHosted"]
 
 
 class organizationForm(NetBoxModelForm):
     model = organization
-    fields = ("orgId", "orgName")
+    fields = ["orgId", "orgName"]
 
 class roaForm(NetBoxModelForm):
     model = roa
-    fields = ("name", "originAs", "validFrom", "validTo", "signedBy")
+    fields = ["name", "originAs", "validFrom", "validTo", "signedBy"]
 
 
 class roapreficesForm(NetBoxModelForm):
     model = roaprefices
-    fields = ("prefix", "maxLength", "roaName")
+    fields = ["prefix", "maxLength", "roaName"]
