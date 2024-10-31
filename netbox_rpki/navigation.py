f03bbe537f285b921dc72367b603f92a00ba44e7
Mencken Davidson <76002183+menckend@users.noreply.github.com>
2024-10-30 23:03:36 -0400

    Update navigation.py

--- netbox_rpki/navigation.py
+++ netbox_rpki/navigation.py
@@ -5,11 +5,11 @@ from netbox.plugins import PluginMenuButton, PluginMenuItem, PluginMenu
 
 _menu_items = (
     PluginMenuItem(
-        link='plugins:netbox_rpki:rpkiorganization_list',
+        link='plugins:netbox_rpki:organization_list',
         link_text='RPKI Organizations',
         buttons=(
             PluginMenuButton(
-                link='plugins:netbox_rpki:rpkiorganization_add',
+                link='plugins:netbox_rpki:organization_add',
                 title='Add',
                 icon_class='mdi mdi-plus-thick',
             ),
@@ -17,11 +17,11 @@ _menu_items = (
     ),
 
     PluginMenuItem(
-        link='plugins:netbox_rpki:rpkicertificate_list',
+        link='plugins:netbox_rpki:certificate_list',
         link_text='RPKI Customer Certificates',
         buttons=(
             PluginMenuButton(
-                link='plugins:netbox_rpki:rpkicertificate_add',
+                link='plugins:netbox_rpki:certificate_add',
                 title='Add',
                 icon_class='mdi mdi-plus-thick',
             ),
@@ -29,11 +29,11 @@ _menu_items = (
     ),
 
     PluginMenuItem(
-        link='plugins:netbox_rpki:rpkiroa_list',
+        link='plugins:netbox_rpki:roa_list',
         link_text='RPKI ROAs',
         buttons=(
             PluginMenuButton(
-                link='plugins:netbox_rpki:rpkiroa_add',
+                link='plugins:netbox_rpki:roa_add',
                 title='Add',
                 icon_class='mdi mdi-plus-thick',
             ),
