408981985ebb7d24b8171636b031bd115febe4d0
Mencken Davidson <76002183+menckend@users.noreply.github.com>
2024-11-05 12:58:05 -0500

    Update forms.py

--- netbox_rpki/forms.py
+++ netbox_rpki/forms.py
@@ -10,28 +10,34 @@ from netbox_rpki.models import Certificate, Organization, Roa, RoaPrefix
 
 
 class CertificateForm(NetBoxModelForm):
+    tenant = DynamicModelChoiceField(queryset=Tenant.objects.all(), required=False)
+    comments = CommentField()
 
     class Meta:
         model = Certificate
-        fields = ['name', 'issuer', 'subject', 'serial', 'valid_from', 'valid_to', "auto_renews", 'public_key', 'private_key', 'publication_url', 'ca_repository', 'rpki_org', 'self_hosted']
+        fields = ['name', 'issuer', 'subject', 'serial', 'valid_from', 'valid_to', "auto_renews", 'public_key', 'private_key', 'publication_url', 'ca_repository', 'rpki_org', 'self_hosted', "tenant']
 
 
 class OrganizationForm(NetBoxModelForm):
-
+    tenant = DynamicModelChoiceField(queryset=Tenant.objects.all(), required=False)
+    comments = CommentField()
+    
     class Meta:
         model = Organization
-        fields = ['org_id', 'name', 'parent_rir', 'ext_url']
+        fields = ['org_id', 'name', 'parent_rir', 'ext_url', 'tenant']
 
 
 class RoaForm(NetBoxModelForm):
-
+    tenant = DynamicModelChoiceField(queryset=Tenant.objects.all(), required=False)
+    comments = CommentField()
     class Meta:
         model = Roa
-        fields: list[str] = ['name', 'origin_as', 'valid_from', 'valid_to', "auto_renews", 'signed_by']
+        fields: list[str] = ['name', 'origin_as', 'valid_from', 'valid_to', "auto_renews", 'signed_by', 'tenant']
 
 
 class RoaPrefixForm(NetBoxModelForm):
-
+    tenant = DynamicModelChoiceField(queryset=Tenant.objects.all(), required=False)
+    comments = CommentField()
     class Meta:
         model = RoaPrefix
-        fields = ['prefix', 'max_length', 'roa_name']
+        fields = ['prefix', 'max_length', 'roa_name', 'tenant']
