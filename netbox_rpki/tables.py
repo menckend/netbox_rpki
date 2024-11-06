a5e4378b0553d40cce54c78d0f52f876f12965be
Mencken Davidson <76002183+menckend@users.noreply.github.com>
2024-11-05 19:47:21 -0500

    Update tables.py

--- netbox_rpki/tables.py
+++ netbox_rpki/tables.py
@@ -27,8 +27,8 @@ class CertificateTable(NetBoxTable):
     )
     class Meta(NetBoxTable.Meta):
         model = netbox_rpki.models.Certificate
-        fields = ("pk", "id", "name", "issuer", "subject", "serial", "valid_from", "valid_to", "auto_renews", "publicKey", "private_key", "publication_url", "ca_repository", "self_hosted", "rpki_org")
-        default_columns = ("name", "issuer", "subject", "serial", "valid_from", "valid_to", "auto_renews", "publicKey", "private_key", "publication_url;", "ca_repository", "self_hosted", "rpki_org")
+        fields = ("pk", "id", "name", "issuer", "subject", "serial", "valid_from", "valid_to", "auto_renews", "publicKey", "private_key", "publication_url", "ca_repository", "self_hosted", "rpki_org", "comments")
+        default_columns = ("name", "issuer", "subject", "serial", "valid_from", "valid_to", "auto_renews", "publicKey", "private_key", "publication_url;", "ca_repository", "self_hosted", "rpki_org", "comments")
 
 
 class OrganizationTable(NetBoxTable):
@@ -42,8 +42,8 @@ class OrganizationTable(NetBoxTable):
 
     class Meta(NetBoxTable.Meta):
         model = netbox_rpki.models.Organization
-        fields = ("pk", "id", "org_id", "name", "parent_rir", "ext_url")
-        default_columns = ("org_id", "name","parent_rir", "ext_url")
+        fields = ("pk", "id", "org_id", "name", "parent_rir", "ext_url", "comments")
+        default_columns = ("org_id", "name","parent_rir", "ext_url", "comments")
 
 
 class RoaTable(NetBoxTable):
@@ -57,8 +57,8 @@ class RoaTable(NetBoxTable):
 
     class Meta(NetBoxTable.Meta):
         model = netbox_rpki.models.Roa
-        fields = ("pk", "id", 'name', "origin_as", "valid_from", "valid_to", "auto_renews", "signed_by")
-        default_columns = ("name", "origin_as", "valid_from", "valid_to", "auto_renews", "signed_by")
+        fields = ("pk", "id", 'name', "origin_as", "valid_from", "valid_to", "auto_renews", "signed_by", "comments")
+        default_columns = ("name", "origin_as", "valid_from", "valid_to", "auto_renews", "signed_by", "comments")
 
 
 class RoaPrefixTable(NetBoxTable):
@@ -72,5 +72,5 @@ class RoaPrefixTable(NetBoxTable):
 
     class Meta(NetBoxTable.Meta):
         model = netbox_rpki.models.RoaPrefix
-        fields = ("pk", "id", "prefix", "max_length", "roa_name")
-        default_columns = ("prefix", "max_length", "roa_name")
+        fields = ("pk", "id", "prefix", "max_length", "roa_name", "comments")
+        default_columns = ("prefix", "max_length", "roa_name", "comments")
