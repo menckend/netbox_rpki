f38c470473b91d9046ed435ba802c30b3a901abe
Mencken Davidson <76002183+menckend@users.noreply.github.com>
2024-11-05 19:56:06 -0500

    Update forms.py

--- netbox_rpki/forms.py
+++ netbox_rpki/forms.py
@@ -43,7 +43,7 @@ class CertificateForm(NetBoxModelForm):
 
     class Meta:
         model = Certificate
-        fields = ['name', 'issuer', 'subject', 'serial', 'valid_from', 'valid_to', "auto_renews", 'public_key', 'private_key', 'publication_url', 'ca_repository', 'rpki_org', 'self_hosted', 'tenant']
+        fields = ['name', 'issuer', 'subject', 'serial', 'valid_from', 'valid_to', "auto_renews", 'public_key', 'private_key', 'publication_url', 'ca_repository', 'rpki_org', 'self_hosted', 'tenant', 'comment']
 
 
 class CertificateFilterForm(NetBoxModelFilterSetForm):
@@ -60,7 +60,7 @@ class OrganizationForm(NetBoxModelForm):
     
     class Meta:
         model = Organization
-        fields = ['org_id', 'name', 'parent_rir', 'ext_url', 'tenant']
+        fields = ['org_id', 'name', 'parent_rir', 'ext_url', 'tenant', 'comment']
 
 
 class RoaForm(NetBoxModelForm):
@@ -68,7 +68,7 @@ class RoaForm(NetBoxModelForm):
     comments = CommentField()
     class Meta:
         model = Roa
-        fields: list[str] = ['name', 'origin_as', 'valid_from', 'valid_to', "auto_renews", 'signed_by', 'tenant']
+        fields: list[str] = ['name', 'origin_as', 'valid_from', 'valid_to', "auto_renews", 'signed_by', 'tenant', 'comment']
 
 
 class RoaPrefixForm(NetBoxModelForm):
@@ -76,4 +76,4 @@ class RoaPrefixForm(NetBoxModelForm):
     comments = CommentField()
     class Meta:
         model = RoaPrefix
-        fields = ['prefix', 'max_length', 'roa_name', 'tenant']
+        fields = ['prefix', 'max_length', 'roa_name', 'tenant', 'comment']
