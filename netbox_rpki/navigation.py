2097256439ecec1fee6ad6eb9ec7dcb93a53e6c0
Mencken Davidson <76002183+menckend@users.noreply.github.com>
2024-11-07 10:23:45 -0500

    Update navigation.py

--- netbox_rpki/navigation.py
+++ netbox_rpki/navigation.py
@@ -85,10 +85,10 @@ if plugin_settings.get('top_level_menu'):
         label="RPKI",
         groups=(
             ("Resources", resource_menu_items),
-        ),
             ("ROAs", roa_menu_items),
-        )
+        ),
         icon_class="mdi mdi-bootstrap"
+    )
 else:
     menu_items = (
         resource_menu_items + roa_menu_items
