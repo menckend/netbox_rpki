a6ef9fc2456b59b15fe7f30e79b8c9675dd5621b
menckend <mencken@gmail.com>
2024-11-18 22:04:43 -0500

    graphql

--- netbox_rpki/graphql/schema.py
+++ netbox_rpki/graphql/schema.py
@@ -1,7 +1,10 @@
-from typing import List
+from typing import Annotated, List
 
 import strawberry
 import strawberry_django
+
+from netbox.graphql.types import NetBoxObjectType
+from netbox.graphql.scalars import BigInt
 
 from netbox_rpki.models import (
     Certificate,
@@ -11,16 +14,39 @@ from netbox_rpki.models import (
     Roa,
     RoaPrefix
 )
-from .types import (
-    CertificateType,
-    CertificateAsnType,
-    CertificatePrefixType,
-    OrganizationType,
-    RoaType,
-    RoaPrefixType
+from .filters import (
+    CertificateFilter,
+    CertificatePrefixFilter,
+    CertificateAsnFilter,
+    RoaFilter,
+    OrganizationFilter,
+    RoaPrefixFilter,
 )
 
 
+@strawberry_django.type(Organization, fields="__all__", filters=OrganizationFilter)
+class OrganizationType(NetBoxObjectType):
+    pass
+
+@strawberry_django.type(Certificate, fields="__all__", filters=CertificateFilter)
+class CertificateType(NetBoxObjectType):
+    pass
+
+@strawberry_django.type(CertificatePrefix, fields="__all__", filters=CertificatePrefixFilter)
+class CertificatePrefixType(NetBoxObjectType):
+    pass
+
+@strawberry_django.type(CertificateAsn, fields="__all__", filters=CertificateAsnFilter)
+class CertificateAsnType(NetBoxObjectType):
+    pass
+
+@strawberry_django.type(Roa, fields="__all__", filters=RoaFilter)
+class RoaType(NetBoxObjectType):
+    pass
+
+@strawberry_django.type(RoaPrefix, fields="__all__", filters=RoaPrefixFilter)
+class RoaPrefixType(NetBoxObjectType):
+    pass
 
 @strawberry.type(name="Query")
 class NetBoxRpkiQuery:
@@ -41,4 +67,4 @@ class NetBoxRpkiQuery:
     netbox_rpki_roa_list: List[RoaType] = strawberry_django.field()
 
     netbox_rpki_roa_prefix: RoaPrefixType = strawberry_django.field()
-    netbox_rpki_roa_list_prefix: List[RoaPrefixType] = strawberry_django.field()
+    netbox_rpki_roa_prefix_list: List[RoaPrefixType] = strawberry_django.field()
