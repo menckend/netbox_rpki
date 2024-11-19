0876ae24357019370f4cba2370bfac59e18c90ca
menckend <mencken@gmail.com>
2024-11-18 22:27:43 -0500

    graphql

--- netbox_rpki/graphql/schema.py
+++ netbox_rpki/graphql/schema.py
@@ -1,10 +1,7 @@
-from typing import Annotated, List
+from typing import List
 
 import strawberry
 import strawberry_django
-
-from netbox.graphql.types import NetBoxObjectType
-from netbox.graphql.scalars import BigInt
 
 from netbox_rpki.models import (
     Certificate,
@@ -14,39 +11,15 @@ from netbox_rpki.models import (
     Roa,
     RoaPrefix
 )
-from .filters import (
-    CertificateFilter,
-    CertificatePrefixFilter,
-    CertificateAsnFilter,
-    RoaFilter,
-    OrganizationFilter,
-    RoaPrefixFilter,
+from .types import (
+    CertificateType,
+    CertificateAsnType,
+    CertificatePrefixType,
+    OrganizationType,
+    RoaType,
+    RoaPrefixType
 )
 
-
-@strawberry_django.type(Organization, fields="__all__", filters=OrganizationFilter)
-class OrganizationType(NetBoxObjectType):
-    pass
-
-@strawberry_django.type(Certificate, fields="__all__", filters=CertificateFilter)
-class CertificateType(NetBoxObjectType):
-    pass
-
-@strawberry_django.type(CertificatePrefix, fields="__all__", filters=CertificatePrefixFilter)
-class CertificatePrefixType(NetBoxObjectType):
-    pass
-
-@strawberry_django.type(CertificateAsn, fields="__all__", filters=CertificateAsnFilter)
-class CertificateAsnType(NetBoxObjectType):
-    pass
-
-@strawberry_django.type(Roa, fields="__all__", filters=RoaFilter)
-class RoaType(NetBoxObjectType):
-    pass
-
-@strawberry_django.type(RoaPrefix, fields="__all__", filters=RoaPrefixFilter)
-class RoaPrefixType(NetBoxObjectType):
-    pass
 
 @strawberry.type(name="Query")
 class NetBoxRpkiQuery:
